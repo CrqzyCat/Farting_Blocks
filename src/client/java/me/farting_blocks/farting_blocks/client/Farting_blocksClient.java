553296b7cd108e399783c19c47b9616c7dc90d88
crqzycat <crqzycat@gmail.com>
2026-04-05 16:30:30 +0200

    Initial commit

---
+++ src/client/java/me/farting_blocks/farting_blocks/client/Farting_blocksClient.java
@@ -0,0 +1,10 @@
+package me.farting_blocks.farting_blocks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Farting_blocksClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/farting_blocks/farting_blocks/Farting_blocks.java
@@ -0,0 +1,10 @@
+package me.farting_blocks.farting_blocks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Farting_blocks implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
